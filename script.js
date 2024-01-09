079a1cb452fa9423edefc02c80fdc3412a5bea95
phogatheena <109434462+phogatheena@users.noreply.github.com>
2024-01-09 16:17:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const extendHex = (shortHex) => {
   // write your code here
+	if(shortHex.length ===4){
+		let a,b,c;
+		a = shortHex[1];
+		b = shortHex[2];
+		c = shortHex[3];
+		let hexcode = "#"+a+a+b+b+c+c;
+		return hexcode;
+	}
+	if(shortHex.length===3){
+		let a,b,c;
+		a = shortHex[0];
+		b = shortHex[1];
+		c= shortHex[2];
+		let hexcode = "#"+a+a+b+b+c+c;
+		return hexcode;
+	}	
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
+//const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
